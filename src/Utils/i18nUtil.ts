bf8f2a01f9f76c90a6bfb0fd5e91b59f28c9d684
Davide Maggi <davide.maggi@eng.it>
2022-04-08 14:33:16 +0200

    changed Lang fileName

--- src/Utils/i18nUtil.ts
+++ src/Utils/i18nUtil.ts
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
-import English from "../assets/i18n/english";
-import Italian from "../assets/i18n/italian";
+import English from "../assets/i18n/en";
+import Italian from "../assets/i18n/it";
 import { changeLanguage } from "../Slices/configSlice";
 import { store,RootState } from "../store";
 import _ from "lodash";
